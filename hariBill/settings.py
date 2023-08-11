65b0b697b33b8904b295ead3391da6c876ca41dd
sanjay <sanjaypython22@gmail.com>
2023-08-11 21:41:56 +0530

    'haribill'

--- hariBill/settings.py
+++ hariBill/settings.py
@@ -68,10 +68,21 @@ WSGI_APPLICATION = 'hariBill.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hariBill',
+        'USER': 'harinam',
+        'PASSWORD': 'qwerty123',
+        'HOST': '68.178.149.80',  # Use the appropriate hostname if not on localhost
+        'PORT': '3306',       # MySQL default port
     }
 }
 
