ab2da771cd9b9ff05ce6b4e3e5c8fcbaa5000368
hariname <142036966+hariname@users.noreply.github.com>
2023-08-23 12:23:30 +0530

    Update views.py

--- homepage/views.py
+++ homepage/views.py
@@ -1,7 +1,7 @@
 import csv
 
 from datetime import datetime
-import time
+
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import redirect
 from django.shortcuts import render
