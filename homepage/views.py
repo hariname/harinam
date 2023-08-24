993a9a70b62eae4e49a7a35c2a84a9de107a1eaf
sanjay <sanjaypython22@gmail.com>
2023-08-24 20:13:50 +0530

    'editbill'

--- homepage/views.py
+++ homepage/views.py
@@ -120,20 +120,75 @@ def view_invoice(request, id=None):
 
 
 def edit_bill(request, bill_id):
-    trans_detail = TransactionHistory.objects.get(id=bill_id)
-    date = trans_detail.date
-    cash_credit = trans_detail.cash_credit
-    party_details = trans_detail.party
+    if request.method == 'POST':
+        form = request.POST
+        bill_id = form.get('bill_id')
+        party = form.get('party')
+        bill_date = form.get('bill_date')
+        bill_date = convert_date(bill_date)
+        product_id = form.getlist('product_id')
+        sale_qty = form.getlist('sale_qty')
+        closeStock = form.getlist('closeStock')
+        price = form.getlist('price')
+        type_discount = form.getlist('type_discount')
+        discount = form.getlist('discount')
+        discount_price = form.getlist('discount_price')
+        subtotal = form.getlist('subtotal')
+        cash_credit = form.get('cash_type')
 
-    items_detail = TransactionDetails.objects.filter(trans_history_id=bill_id)
-    context = {
-        'bill_id': bill_id,
-        'date': date,
-        'cash_credit': cash_credit,
-        'party_details': party_details,
-        'items_detail': items_detail,
-    }
-    return render(request, 'edit_bill.html', context)
+        trans_id = TransactionHistory.objects.filter(id=bill_id).update(invoice_number=str(datetime.now().strftime("%H%M%S")),
+                                                                        party_id=party,
+                                                                        cash_credit=cash_credit,
+                                                                        date=bill_date)
+
+        TransactionDetails.objects.filter(trans_history_id=bill_id).delete()
+        if trans_id:
+            for i in range(len(product_id)):
+                product_data = Product.objects.get(id=product_id[i])
+                open_stock = int(product_data.open_stock)
+                present_stock = product_data.present_stock - int(sale_qty[i])
+                pur_rate = int(product_data.purchase_price)
+                sale_price = int(product_data.sale_price)
+                sale_amt = int(sale_qty[i]) * sale_price
+                # pur_amt = int(sale_qty[i]) * pur_rate
+                pur_amt = present_stock * pur_rate
+
+                TransactionDetails.objects.create(trans_history_id=bill_id,
+                                                  product_id=product_id[i],
+                                                  sale_qty=sale_qty[i],
+                                                  base_price=price[i],
+                                                  discount_type=type_discount[i],
+                                                  discount=discount[i],
+                                                  discount_price=discount_price[i],
+                                                  net_sale=subtotal[i],
+                                                  pur_rate=pur_rate,
+                                                  sale_rate=sale_price,
+                                                  open_stock=open_stock,
+                                                  close_stock=present_stock,
+                                                  sale_amt=sale_amt,
+                                                  pur_amt=pur_amt,
+                                                  date=bill_date,
+                                                  )
+
+                Product.objects.filter(id=product_id[i]).update(present_stock=present_stock)
+            return redirect(f'/view-invoice/{bill_id}/')
+
+    else:
+        trans_detail = TransactionHistory.objects.get(id=bill_id)
+        date = trans_detail.date
+        cash_credit = trans_detail.cash_credit
+        party_details = trans_detail.party
+
+        items_detail = TransactionDetails.objects.filter(trans_history_id=bill_id)
+        print(items_detail,'=====items_detail')
+        context = {
+            'bill_id': bill_id,
+            'date': date,
+            'cash_credit': cash_credit,
+            'party_details': party_details,
+            'items_detail': items_detail,
+        }
+        return render(request, 'edit_bill.html', context)
 
 
 def transactionHISTORY(request):
