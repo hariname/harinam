aa733b13edd80d16609e8486c5fffaf20cc7528d
sanjay <sanjaypython22@gmail.com>
2023-08-25 23:32:29 +0530

    'i'

--- homepage/views.py
+++ homepage/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import render
 from django.template.loader import get_template
 from party.models import Party
 from product.models import Product, TransactionHistory, TransactionDetails
-from xhtml2pdf import pisa
+# from xhtml2pdf import pisa
 from io import BytesIO
 
 def convert_date(date_string, output_format='%Y-%m-%d'):
@@ -310,88 +310,88 @@ def get_user(request):
 #             return HttpResponse("Invalid PDF", status_code=400, content_type='text/plain')
 #         return HttpResponse(result.getvalue(), content_type='application/pdf')
 
-
-def render_to_pdf(template_src, context_dict={}):
-    template = get_template(template_src)
-    html = template.render(context_dict)
-    result = BytesIO()
-    pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result)
-    if not pdf.err:
-        return HttpResponse(result.getvalue(), content_type='application/pdf')
-    return None
-
-
-def generate_pdf(request, id):
-    if id is not None:
-        invoice_detail = TransactionHistory.objects.get(id=id)
-        party = invoice_detail.party
-        party_add = invoice_detail.party.address
-        party_name = invoice_detail.party.customer_name
-        cash_credit = invoice_detail.cash_credit
-        invoice_no = invoice_detail.invoice_number
-        invoice_date = invoice_detail.date
-        invoice = TransactionDetails.objects.filter(trans_history_id=id)
-        data_list = []
-        total_price = 0
-        for i in invoice:
-            data_dict = {}
-            data_dict['invoice'] = i.trans_history.invoice_number
-            data_dict['code'] = i.product.code
-            data_dict['product_name'] = i.product.product_name
-            data_dict['sale_qty'] = i.sale_qty
-            data_dict['base_price'] = i.base_price
-            data_dict['discount_type'] = i.discount_type
-            data_dict['discount'] = i.discount
-            data_dict['net_sale'] = i.net_sale
-            data_dict['sale_rate'] = i.sale_rate
-            data_dict['sale_amt'] = i.sale_amt
-            data_dict['date'] = i.date
-            data_dict['party'] = i.trans_history.party
-            total_price += data_dict['net_sale']
-
-            data_list.append(data_dict)
-
-        context = {
-            'bill_id': id,
-            'cash_credit': cash_credit,
-            'party': party,
-            'party_add': party_add,
-            'recipient_name': party_name,
-            'invoice_no': invoice_no,
-            'invoice_date': invoice_date,
-            'data_list': data_list,
-            'invoice': invoice,
-            'total_price': total_price,
-        }
-    pdf = render_to_pdf('render_pdf.html', context)
-    if pdf:
-        response = HttpResponse(pdf, content_type='application/pdf')
-        filename = "your_Bill.pdf"  # Change the filename accordingly
-        response['Content-Disposition'] = f'attachment; filename="{filename}"'
-        return response
-    return HttpResponse("Error generating PDF")
-
-
-def send_email(request, id):
-    invoice_detail = TransactionHistory.objects.get(id=id)
-    party = invoice_detail.party
-    party_email = invoice_detail.party.email
-    party_name = invoice_detail.party.customer_name
-    email_subject = 'Your Bill Generated'
-
-    # Generate the PDF using the PDF view
-    pdf_response = generate_pdf(request, id)
-    if pdf_response:
-        pdf_content = pdf_response.content
-
-        # Create an EmailMessage instance
-        email = EmailMessage(email_subject, f"HELLO, {party_name}", str(party_email), [party_email])
-        email.attach("your_pdf_filename.pdf", pdf_content, 'application/pdf')  # Attach the PDF
-
-        try:
-            email.send()
-            return HttpResponse("Email sent successfully")
-        except Exception as e:
-            return HttpResponse(f"An error occurred: {str(e)}")
-    else:
-        return HttpResponse("Error generating PDF")
+#
+# def render_to_pdf(template_src, context_dict={}):
+#     template = get_template(template_src)
+#     html = template.render(context_dict)
+#     result = BytesIO()
+#     pdf = pisa.pisaDocument(BytesIO(html.encode("UTF-8")), result)
+#     if not pdf.err:
+#         return HttpResponse(result.getvalue(), content_type='application/pdf')
+#     return None
+#
+#
+# def generate_pdf(request, id):
+#     if id is not None:
+#         invoice_detail = TransactionHistory.objects.get(id=id)
+#         party = invoice_detail.party
+#         party_add = invoice_detail.party.address
+#         party_name = invoice_detail.party.customer_name
+#         cash_credit = invoice_detail.cash_credit
+#         invoice_no = invoice_detail.invoice_number
+#         invoice_date = invoice_detail.date
+#         invoice = TransactionDetails.objects.filter(trans_history_id=id)
+#         data_list = []
+#         total_price = 0
+#         for i in invoice:
+#             data_dict = {}
+#             data_dict['invoice'] = i.trans_history.invoice_number
+#             data_dict['code'] = i.product.code
+#             data_dict['product_name'] = i.product.product_name
+#             data_dict['sale_qty'] = i.sale_qty
+#             data_dict['base_price'] = i.base_price
+#             data_dict['discount_type'] = i.discount_type
+#             data_dict['discount'] = i.discount
+#             data_dict['net_sale'] = i.net_sale
+#             data_dict['sale_rate'] = i.sale_rate
+#             data_dict['sale_amt'] = i.sale_amt
+#             data_dict['date'] = i.date
+#             data_dict['party'] = i.trans_history.party
+#             total_price += data_dict['net_sale']
+#
+#             data_list.append(data_dict)
+#
+#         context = {
+#             'bill_id': id,
+#             'cash_credit': cash_credit,
+#             'party': party,
+#             'party_add': party_add,
+#             'recipient_name': party_name,
+#             'invoice_no': invoice_no,
+#             'invoice_date': invoice_date,
+#             'data_list': data_list,
+#             'invoice': invoice,
+#             'total_price': total_price,
+#         }
+#     pdf = render_to_pdf('render_pdf.html', context)
+#     if pdf:
+#         response = HttpResponse(pdf, content_type='application/pdf')
+#         filename = "your_Bill.pdf"  # Change the filename accordingly
+#         response['Content-Disposition'] = f'attachment; filename="{filename}"'
+#         return response
+#     return HttpResponse("Error generating PDF")
+#
+#
+# def send_email(request, id):
+#     invoice_detail = TransactionHistory.objects.get(id=id)
+#     party = invoice_detail.party
+#     party_email = invoice_detail.party.email
+#     party_name = invoice_detail.party.customer_name
+#     email_subject = 'Your Bill Generated'
+#
+#     # Generate the PDF using the PDF view
+#     pdf_response = generate_pdf(request, id)
+#     if pdf_response:
+#         pdf_content = pdf_response.content
+#
+#         # Create an EmailMessage instance
+#         email = EmailMessage(email_subject, f"HELLO, {party_name}", str(party_email), [party_email])
+#         email.attach("your_pdf_filename.pdf", pdf_content, 'application/pdf')  # Attach the PDF
+#
+#         try:
+#             email.send()
+#             return HttpResponse("Email sent successfully")
+#         except Exception as e:
+#             return HttpResponse(f"An error occurred: {str(e)}")
+#     else:
+#         return HttpResponse("Error generating PDF")
