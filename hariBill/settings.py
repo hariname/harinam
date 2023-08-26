6f09b4f028ba601181335e4bd5fa5067103a165f
sanjay <sanjaypython22@gmail.com>
2023-08-26 09:28:09 +0530

    'kj'

--- hariBill/settings.py
+++ hariBill/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
+EMAIL_PORT = 465
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'sanjay.solutions2023@gmail.com'
