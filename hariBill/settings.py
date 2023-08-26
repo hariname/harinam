a64e3d2750bb8ea70dd8186b300997c6391719a3
sanjay <sanjaypython22@gmail.com>
2023-08-26 11:03:54 +0530

    'f'

--- hariBill/settings.py
+++ hariBill/settings.py
@@ -139,9 +139,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtpout.secureserver.net'
-EMAIL_PORT = 587
+EMAIL_PORT = 80
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'info@sanjay.solutions.com'
 EMAIL_HOST_PASSWORD = 'Radharani@123'
-DEFAULT_FROM_EMAIL = 'info@sanjay.solutions.com'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+DEFAULT_ACCESS_TOKEN_LIFE_TIME = 7
