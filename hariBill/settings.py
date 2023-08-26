f5f95ca264ad1bd2f00cbb124aa3c6fa756492ca
sanjay <sanjaypython22@gmail.com>
2023-08-26 09:12:26 +0530

    'kj'

--- hariBill/settings.py
+++ hariBill/settings.py
@@ -133,7 +133,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-# EMAIL_USE_SSL = False
+EMAIL_USE_SSL = False
 EMAIL_HOST_USER = 'sanjay.solutions2023@gmail.com'
 EMAIL_HOST_PASSWORD = 'mgknzbdobjrvysle'
 
